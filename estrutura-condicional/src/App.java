28a987fe2ff83430f348ac46697d17421fee80ac
Julio Machado <jsmacbusiness@gmail.com>
2023-09-26 17:01:07 -0300

    Feat

--- estrutura-condicional/src/App.java
+++ estrutura-condicional/src/App.java
@@ -81,6 +81,43 @@ public class App {
             System.out.println("Item não encontrado. Verifique o código do item.");
         }
 
+        /*
+         * Faça um programa onde dependendo da entrada do usuário, determina um dia da
+         * semana.
+         */
+
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        String dia;
+
+        switch (x) {
+            case 1:
+                dia = "Domingo";
+                break;
+            case 2:
+                dia = "Segunda";
+                break;
+            case 3:
+                dia = "Terça";
+                break;
+            case 4:
+                dia = "Quarta";
+                break;
+            case 5:
+                dia = "Quinta";
+                break;
+            case 6:
+                dia = "Sexta";
+                break;
+            case 7:
+                dia = "Sábado";
+                break;
+            default:
+                dia = "Valor Inválido";
+                break;
+        }
+        System.out.println("Dia da semana: " + dia);
+        scan.close();
     }
 
     public static void imprimirSinal(int numero) {
