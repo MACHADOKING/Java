db3c8282494d3f1d1b57d74feef1a20073a84159
Julio Machado <jsmacbusiness@gmail.com>
2023-09-29 17:32:29 -0300

    Feat: Estaticos

---
+++ POO/src/estaticos/Program.java
@@ -0,0 +1,44 @@
+package estaticos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import estaticos.util.Calculator;
+
+public class Program {
+
+    // public static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        // Calculator calc = new Calculator();
+
+        System.out.println("Enter radius:");
+        double radius = sc.nextDouble();
+
+        // double c = calc.circumference(radius);
+        double c = Calculator.circumference(radius);
+
+        // double v = calc.volume(radius);
+        double v = Calculator.volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        // System.out.printf("PI value: %.2f%n", calc.PI);
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
+        sc.close();
+    }
+
+    /*
+     * public static double circumference(double radius) {
+     * return 2 * PI * radius;
+     * }
+     * 
+     * public static double volume(double radius) {
+     * return 4.0 * PI * radius * radius * radius / 3;
+     * }
+     */
+
+}
