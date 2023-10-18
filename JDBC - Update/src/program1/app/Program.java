cd1e8b11442f1026d7cd2b794a5672c71f93ef7f
Julio Machado <jsmacbusiness@gmail.com>
2023-10-18 13:11:31 -0300

    Feat: JDBC - Update

---
+++ JDBC - Update/src/program1/app/Program.java
@@ -0,0 +1,35 @@
+//package program1.app;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+        Connection conn = null;
+        PreparedStatement st = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentId = ?)"
+            );
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+}
