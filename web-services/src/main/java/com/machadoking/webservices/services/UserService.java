30cd3112f48ee7f4e2238f1a7c3a354a5d2a420b
Julio Machado <jsmacbusiness@gmail.com>
2023-10-20 15:17:18 -0300

    Feat: Exception handling - delete

--- web-services/src/main/java/com/machadoking/webservices/services/UserService.java
+++ web-services/src/main/java/com/machadoking/webservices/services/UserService.java
@@ -2,8 +2,11 @@ package com.machadoking.webservices.services;
 
 import com.machadoking.webservices.entities.User;
 import com.machadoking.webservices.repositories.UserRepository;
+import com.machadoking.webservices.services.exceptions.DatabaseException;
 import com.machadoking.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
