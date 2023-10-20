73843ac31dff7f18415e8c1388ec80e6ea6eb13b
Julio Machado <jsmacbusiness@gmail.com>
2023-10-20 15:06:54 -0300

    Feat: Exception handling - findById

--- web-services/src/main/java/com/machadoking/webservices/services/UserService.java
+++ web-services/src/main/java/com/machadoking/webservices/services/UserService.java
@@ -2,6 +2,7 @@ package com.machadoking.webservices.services;
 
 import com.machadoking.webservices.entities.User;
 import com.machadoking.webservices.repositories.UserRepository;
+import com.machadoking.webservices.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
