639257d9e3356ea4e95d0545cedfa74e3f0d5df4
Julio Machado <jsmacbusiness@gmail.com>
2023-10-20 15:20:46 -0300

    Feat: Exception handling - update

--- web-services/src/main/java/com/machadoking/webservices/services/UserService.java
+++ web-services/src/main/java/com/machadoking/webservices/services/UserService.java
@@ -4,6 +4,7 @@ import com.machadoking.webservices.entities.User;
 import com.machadoking.webservices.repositories.UserRepository;
 import com.machadoking.webservices.services.exceptions.DatabaseException;
 import com.machadoking.webservices.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
