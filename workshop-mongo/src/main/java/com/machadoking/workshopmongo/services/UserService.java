380877e42cfa382620ca77db488d58627d9aa5da
Julio Machado <jsmacbusiness@gmail.com>
2023-10-23 14:43:58 -0300

    Feat: User insertion per post

--- workshop-mongo/src/main/java/com/machadoking/workshopmongo/services/UserService.java
+++ workshop-mongo/src/main/java/com/machadoking/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.machadoking.workshopmongo.services;
 
 import com.machadoking.workshopmongo.domain.User;
+import com.machadoking.workshopmongo.dto.UserDTO;
 import com.machadoking.workshopmongo.repository.UserRepository;
 import com.machadoking.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
