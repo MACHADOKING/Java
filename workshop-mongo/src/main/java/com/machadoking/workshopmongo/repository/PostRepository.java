5d6e0c10e1bba7c857dc99674bf1bae7a57797f3
Julio Machado <jsmacbusiness@gmail.com>
2023-10-23 17:18:15 -0300

    Feat: Creating entity post with nested user

---
+++ workshop-mongo/src/main/java/com/machadoking/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.machadoking.workshopmongo.repository;
+
+import com.machadoking.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
