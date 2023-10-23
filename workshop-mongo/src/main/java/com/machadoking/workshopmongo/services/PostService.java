79d2cb6285faa1ab935ec7f9ee57d91dc4507b83
Julio Machado <jsmacbusiness@gmail.com>
2023-10-23 17:42:02 -0300

    Feat: Getting post by id

---
+++ workshop-mongo/src/main/java/com/machadoking/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.machadoking.workshopmongo.services;
+
+import com.machadoking.workshopmongo.domain.Post;
+import com.machadoking.workshopmongo.repository.PostRepository;
+import com.machadoking.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
