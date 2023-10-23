3a2235599a13710b3c5b6b170ee5dbde1eba1a2c
Julio Machado <jsmacbusiness@gmail.com>
2023-10-23 17:50:36 -0300

    Feat: Adding comments to posts

--- workshop-mongo/src/main/java/com/machadoking/workshopmongo/config/Instantiation.java
+++ workshop-mongo/src/main/java/com/machadoking/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.machadoking.workshopmongo.config;
 import com.machadoking.workshopmongo.domain.Post;
 import com.machadoking.workshopmongo.domain.User;
 import com.machadoking.workshopmongo.dto.AuthorDTO;
+import com.machadoking.workshopmongo.dto.CommentDTO;
 import com.machadoking.workshopmongo.repository.PostRepository;
 import com.machadoking.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!!", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha uma ótima semana!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
