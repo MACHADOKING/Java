2e2697c7c7e7fcbdb6a35bb1e806a44359cb8c29
Julio Machado <jsmacbusiness@gmail.com>
2023-10-23 17:26:08 -0300

    Feat: Projection of author data with DTO

--- workshop-mongo/src/main/java/com/machadoking/workshopmongo/config/Instantiation.java
+++ workshop-mongo/src/main/java/com/machadoking/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.machadoking.workshopmongo.config;
 
 import com.machadoking.workshopmongo.domain.Post;
 import com.machadoking.workshopmongo.domain.User;
+import com.machadoking.workshopmongo.dto.AuthorDTO;
 import com.machadoking.workshopmongo.repository.PostRepository;
 import com.machadoking.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!!", "Vou viajar para SP, abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!!", "Vou viajar para SP, abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
