5bc157f74c4bef89aa3b1fc063c9ef59aaffa529
Julio Machado <jsmacbusiness@gmail.com>
2023-10-25 19:11:20 -0300

    Feat: DepartmentService

--- javafx/src/main/java/com/javafxprojeto01/javafx/DepartmentListController.java
+++ javafx/src/main/java/com/javafxprojeto01/javafx/DepartmentListController.java
@@ -1,6 +1,9 @@
 package com.javafxprojeto01.javafx;
 
 import com.javafxprojeto01.javafx.model.entities.Department;
+import com.javafxprojeto01.javafx.model.services.DepartmentService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -10,9 +13,12 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DepartmentListController implements Initializable {
+    private DepartmentService service;
+
     @FXML
     private TableView<Department> tableViewDepartment;
 
@@ -25,9 +31,15 @@ public class DepartmentListController implements Initializable {
     @FXML
     private Button btNew;
 
+    private ObservableList<Department> obsList;
+
     @FXML
     public void onBtNewAction() {
         System.out.println("onBtNewAction");
+    }
+
+    public void setDepartmentService(DepartmentService service) {
+        this.service = service;
     }
 
     @Override
@@ -41,5 +53,15 @@ public class DepartmentListController implements Initializable {
 
         Stage stage = (Stage) Main.getMainScene().getWindow();
         tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+    }
+
+    public void updateTableView() {
+        if (service == null) {
+            throw new IllegalStateException("Service was null");
+        }
+
+        List<Department> list = service.findAll();
+        obsList = FXCollections.observableArrayList(list);
+        tableViewDepartment.setItems(obsList);
     }
 }
---
+++ javafx/src/main/java/com/javafxprojeto01/javafx/model/services/DepartmentService.java
@@ -0,0 +1,16 @@
+package com.javafxprojeto01.javafx.model.services;
+
+import com.javafxprojeto01.javafx.model.entities.Department;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepartmentService {
+    public List<Department> findAll() {
+        List<Department> list = new ArrayList<>();
+        list.add(new Department(1, "Books"));
+        list.add(new Department(2, "Computers"));
+        list.add(new Department(3, "Electronics"));
+        return list;
+    }
+}
